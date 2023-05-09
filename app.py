3bf67b0752ba9535c8a4a902e00c29036864a9ca
connorm400 <125999884+connorm400@users.noreply.github.com>
2023-05-09 10:19:05 -0600

    added webpages etc

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -18,11 +18,21 @@ class entry(db.Model):
 def index():
     return render_template('index.html')
 
-'''
-@app.route('/submit', methods=['POST', 'GET'])
+@app.route('/dropper', methods=['POST', 'GET'])
 def submit():
     if request.method == 'POST':
-'''
+        submission_content = request.form['content'] # extracting data from the form
+        new_submission = entry(content=submission_content) 
+
+        try: 
+            db.session.add(new_submission) # adding the task extracted from the form to the database and commiting to save
+            db.session.commit()
+            return redirect('/') # redirecting to the index; since this would be a GET method thingy it would be served the html document
+        except: # if there is ever an error itl show this message
+            return 'there was an issue making your submission'
+    elif request.method == 'GET':
+        return render_template('dropper.html')
+
 
 if __name__ == "__main__":
     with app.app_context():
